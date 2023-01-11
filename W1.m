1294168a7863b2a6a2ff952838baa057b69b3696
pbca <pbca@eriksholm.com>
2023-01-11 09:25:50 +0100

    Added new script to check Utterance and audio synchronization

--- W1.m
+++ W1.m
@@ -1,6 +1,6 @@
-%% PBCA-Thesis-W1
+%% PBCA-Thesis - Week 1
 % Pathing
-% clear all; clc; close all;
+clear all; clc; close all;
 BPath = strsplit(pwd,'PBCA-thesis');
 addpath(genpath('data\'));
 addpath([BPath{1} 'Pupil-preprocessing-tools\tools']) % For preprocessing
@@ -19,14 +19,20 @@ PairFiles=dir(['data\Main',sprintf('%d',PairIn),'\*.mat']);
 PairUtt=load('data\utterances1110.mat');
 PairUtt=PairUtt.Utterances(PairIn,:);
 
-% Parameters
+% Parameters for processing
 Param.Fs = 50; % Sampling frequency of pupil data
 Param.RemoveBeforeAndAfter = [35 100]*1e-3; % Samples within the time range before and after NaNs will set NaNs as well.
 Param.MinLengthNaNRepair = 5; % Drop values (i.e., change to NaN) before and after NaNs only for contiguous NaNs of at least __ samples. 
 LPWinSize = 0.75; % [s]: Window size of hamming-window for low-pass filtering
 LPWindow = hamming(round(LPWinSize*Param.Fs));
 LPWindow = LPWindow/sum(LPWindow); % Hamming-window
-
+TimeStartW=1; % [s], time before Utt/Lis starts
+TimeEndW=2; % [s], time after Utt/Lis starts
+TimeStart=20; % [s], time at which simultaneous recording started
+TimeBL=[5,15]; % [s], time chosen for baseline
+AvgPupilSize = zeros(1,numel(PairFiles)); % [mm], avg pupil size (per file)
+AvgPupilSlope = zeros(1,numel(PairFiles)); % [mm/s], avg pupil slope (per file)
+AvgTimeUtt = zeros(1,numel(PairFiles)); % [s], avg time of Utterance (per file)
 % Run once per file
 for i=1:numel(PairFiles)
     alldata(i) = load([PairFiles(i).folder, '\', PairFiles(i).name]);
@@ -54,6 +60,9 @@ for i=1:numel(PairFiles)
         eyeChosen = 'Right';
     end
     
+    % Baseline the Diameter (from start to finish)
+    BLDiam = mean(Diameter(TimeBL(1)*Param.Fs:TimeBL(2)*Param.Fs))-Diameter;
+    
     % Retrieve Utterances
     if contains(PairFiles(i).name,'P1')
         SpeakKey = 'utteranceCH1';
@@ -69,13 +78,13 @@ for i=1:numel(PairFiles)
         UttB = 1;
     end
     
-    if contains(PairFiles(i).name,'Noise60')
+    if contains(PairFiles(i).name,'Quiet')
         UttCond = UttB + 1;
-    elseif contains(PairFiles(i).name,'Noise70')
-        UttCond = UttB + 3;
-    elseif contains(PairFiles(i).name,'Quiet')
-        UttCond = UttB + 5;
     elseif contains(PairFiles(i).name,'SHL')
+        UttCond = UttB + 3;
+    elseif contains(PairFiles(i).name,'Noise60')
+        UttCond = UttB + 5;
+    elseif contains(PairFiles(i).name,'Noise70')
         UttCond = UttB + 7;
     end
     
@@ -84,34 +93,46 @@ for i=1:numel(PairFiles)
     binRes = PairUtt{1,UttCond}.binRes;
     
     % Downsample (rounding) Utt from 250 Hz (1/binRes) to 50 Hz
-    Speak(:,2:3)=round(Speak(:,2:3)*binRes*Param.Fs);
-    Listen(:,2:3)=round(Listen(:,2:3)*binRes*Param.Fs);
+    Speak(:,2:3)=round(Speak(:,2:3)*binRes*Param.Fs+TimeStart*Param.Fs);
+    Listen(:,2:3)=round(Listen(:,2:3)*binRes*Param.Fs+TimeStart*Param.Fs);
     
     % Time-locked indexes (based on Start or End of events)
-    TimeStartW=1; % [S], time previous to Utt/Lis starts
-    TimeEndW=2; % [S], time after Utt/Lis starts
-    SWSpeakIdx=[Speak(:,2)-TimeStartW*Param.Fs,Speak(:,2),Speak(:,2)+TimeEndW*Param.Fs];
-    SWListenIdx=[Listen(:,2)-TimeStartW*Param.Fs,Listen(:,2),Listen(:,2)+TimeEndW*Param.Fs];
-    EWSpeakIdx=[Speak(:,3)-TimeStartW*Param.Fs,Speak(:,3),Speak(:,3)+TimeEndW*Param.Fs];
-    EWListenIdx=[Listen(:,3)-TimeStartW*Param.Fs,Listen(:,3),Listen(:,3)+TimeEndW*Param.Fs];
+%     SWSpeakIdx=[Speak(:,2)-TimeStartW*Param.Fs,Speak(:,2),Speak(:,2)+TimeEndW*Param.Fs];
+%     SWListenIdx=[Listen(:,2)-TimeStartW*Param.Fs,Listen(:,2),Listen(:,2)+TimeEndW*Param.Fs];
+%     EWSpeakIdx=[Speak(:,3)-TimeStartW*Param.Fs,Speak(:,3),Speak(:,3)+TimeEndW*Param.Fs];
+%     EWListenIdx=[Listen(:,3)-TimeStartW*Param.Fs,Listen(:,3),Listen(:,3)+TimeEndW*Param.Fs];
     
-    % Time vectors for plotting
-    t_LDiam = linspace(1,length(LDiam)./Param.Fs,length(LDiam));
-    t_RDiam = linspace(1,length(RDiam)./Param.Fs,length(RDiam));
+    % Time vectors and idx for plotting
+    t_Diam = linspace(1,length(BLDiam)./Param.Fs,length(BLDiam));
+    startStopU = t_Diam(Speak(:,2:3)); 
+    widthU = startStopU(:,2)-startStopU(:,1);
+    startStopL = t_Diam(Listen(:,2:3)); 
+    widthL = startStopL(:,2)-startStopL(:,1);
+    
+    % Features (of each window): mean slope, mean pupil size
+    if ~isempty(Speak)
+        for j=1:size(Speak,1)
+            AvgPupilSize(i)=AvgPupilSize(i)+mean(Diameter(Speak(j,2):Speak(j,3)));
+            AvgPupilSlope(i)=AvgPupilSlope(i)+...
+                mean(diff(Diameter(Speak(j,2):Speak(j,3)))./diff(t_Diam(Speak(j,2):Speak(j,3)))');
+        end
+        AvgPupilSize(i)=AvgPupilSize(i)./j;
+        AvgPupilSlope(i)=AvgPupilSlope(i)./j;
+    else
+        AvgPupilSize(i)=NaN;
+        AvgPupilSlope(i)=NaN;
+    end
+    % Global mean of utterance per file
+    AvgTimeUtt(i) = mean(Speak(:,1)); % Possible NaNs
     
     % Plots
-    
     figure
-    subplot(2,2,[1 2])
-    Lplot=plot(t_LDiam,LDiam,color='blue');
+%     subplot(2,2,[1 2])
+    plot(t_Diam,BLDiam,color='black');
     hold on
-    Rplot=plot(t_RDiam,RDiam,color='red');
+    xline(TimeStart,"--",'HandleVisibility','off')
     yl=ylim();
     ylim(yl);
-    startStopU = t_LDiam(Speak(:,2:3)); 
-    widthU = startStopU(:,2)-startStopU(:,1);
-    startStopL = t_LDiam(Listen(:,2:3)); 
-    widthL = startStopL(:,2)-startStopL(:,1);
     % Plot rectangles (Utterance and listening time windows)
     arrayfun(@(i)rectangle('Position', [startStopU(i,1),yl(1),widthU(i),range(yl)], ...
     'EdgeColor', 'none', 'FaceColor', [0 1 0 .2]), 1:size(startStopU,1))
@@ -120,51 +141,64 @@ for i=1:numel(PairFiles)
     if ~isempty(Speak)
         eline1=line(NaN,NaN,'LineWidth',2,'Color',[0 1 0 .2]);
         eline2=line(NaN,NaN,'LineWidth',2,'Color',[1 0 1 .2]);
-        legend([Lplot Rplot eline1 eline2],{'Diameter Left','Diameter Right','Utterance windows','Listening windows'})
+        legend(['Baselined diameter (', eyeChosen,' Eye)'],'Utterance windows','Listening windows')
     else
         disp(['Warning: No associated Utterance/Listening data for file ', PairFiles(i).name, '.']);
-        legend([Lplot Rplot],{'Diameter Left','Diameter Right'})
+        legend(['Baselined diameter (', eyeChosen,' Eye)'])
     end
     sgtitle(strrep(PairFiles(i).name,'_','-'))
-    xticks([0:30:t_LDiam(end)])
+    xticks([0:TimeStart:t_Diam(end)])
     xlabel('Time [s]')
-    ylabel('Pupil diameter [mm]')
+    ylabel('Pupil baseline difference [mm]');
     
-    subplot(2,2,3)
-    % Start/End Diameter Sum (for averaging)
-    SDSum=zeros(1,Param.Fs*(TimeEndW+TimeStartW)); 
-    EDSum=SDSum;
-    if ~isempty(Speak)
-        hold on
-        for j=1:size(Speak,1)
-            plot(linspace(-TimeStartW,TimeEndW,Param.Fs*(TimeEndW+TimeStartW)),...
-                 Diameter(SWSpeakIdx(j,1):SWSpeakIdx(j,3)-1),color=[0 1 0 .2],LineWidth=0.3)
-            SDSum=SDSum+Diameter(SWSpeakIdx(j,1):SWSpeakIdx(j,3)-1);
-        end
-        plot(linspace(-TimeStartW,TimeEndW,Param.Fs*(TimeEndW+TimeStartW)),...
-             SDSum./size(Speak,1),color=[1 0 0 0.8],LineWidth=1)
-        xline(0,"--")
-        xticks([-TimeStartW:1:TimeEndW])
-        title('Utterance-evoked pupil response')
-        xlabel('Time [s]')
-        ylabel('Pupil diameter [mm]')
-    end
-    
-    subplot(2,2,4)
-    if ~isempty(Listen)
-        hold on
-        for j=1:size(Listen,1)
-            plot(linspace(-TimeStartW,TimeEndW,Param.Fs*(TimeEndW+TimeStartW)),...
-                 Diameter(SWListenIdx(j,1):SWListenIdx(j,3)-1),color=[1 0 1 0.2],LineWidth=0.3)
-             EDSum=EDSum+Diameter(SWListenIdx(j,1):SWListenIdx(j,3)-1);
-        end
-        plot(linspace(-TimeStartW,TimeEndW,Param.Fs*(TimeEndW+TimeStartW)),...
-             EDSum./size(Listen,1),color=[1 0 0 0.8],LineWidth=1)
-        xline(0,"--")
-        xticks([-TimeStartW:1:TimeEndW])
-        title('Listening-evoked pupil response')
-        xlabel('Time [s]')
-        ylabel('Pupil diameter [mm]')
-    end
+%     subplot(2,2,3)
+%     % Start/End Diameter Sum (for averaging)
+%     SDWSum = zeros(1,Param.Fs*(TimeEndW+TimeStartW)); % Sum Speak-Diam-Window
+%     k=0;
+%     if ~isempty(Speak)
+%         hold on
+%         for j=1:size(Speak,1)
+%             if SWSpeakIdx(j,3)-1 <= length(BLDiam)
+%                 plot(linspace(-TimeStartW,TimeEndW,Param.Fs*(TimeEndW+TimeStartW)),...
+%                     BLDiam(SWSpeakIdx(j,1):SWSpeakIdx(j,3)-1),color=[0 1 0 .2],LineWidth=0.3)
+%                 k=k+1;
+%                 SDWSum=SDWSum+BLDiam(SWSpeakIdx(j,1):SWSpeakIdx(j,3)-1);
+%             end
+%         end
+%         plot(linspace(-TimeStartW,TimeEndW,Param.Fs*(TimeEndW+TimeStartW)),...
+%              SDWSum./k,color=[1 0 0 0.8],LineWidth=1.5)
+%         xline(0,"--")
+%         xticks([-TimeStartW:1:TimeEndW])
+%         title('Baselined utterance-evoked pupil response')
+%         xlabel('Time [s]')
+%         ylabel('Pupil baseline difference [mm]');
+%     end
+%     
+%     subplot(2,2,4)
+%     LDWSum = zeros(1,Param.Fs*(TimeEndW+TimeStartW)); % Sum Listen-Diam-Window
+%     k=0;
+%     if ~isempty(Listen)
+%         hold on
+%         for j=1:size(Listen,1)
+%             if SWListenIdx(j,3)-1 <= length(BLDiam)
+%                 plot(linspace(-TimeStartW,TimeEndW,Param.Fs*(TimeEndW+TimeStartW)),...
+%                      BLDiam(SWListenIdx(j,1):SWListenIdx(j,3)-1),color=[1 0 1 0.2],LineWidth=0.3)
+%                 k=k+1;
+%                 LDWSum=LDWSum+BLDiam(SWListenIdx(j,1):SWListenIdx(j,3)-1);
+%             end
+%         end
+%         plot(linspace(-TimeStartW,TimeEndW,Param.Fs*(TimeEndW+TimeStartW)),...
+%              LDWSum./k,color=[1 0 0 0.8],LineWidth=1.5)
+%         xline(0,"--")
+%         xticks([-TimeStartW:1:TimeEndW])
+%         title('Baselined listening-evoked pupil response')
+%         xlabel('Time [s]')
+%         ylabel('Pupil baseline difference [mm]');
+%     end
     
 end
+
+% Global paramters
+GlobalAvgPupilSize = mean(AvgPupilSize,'omitnan');
+GlobalAvgPupilSlope = mean(AvgPupilSlope,'omitnan');
+GlobalAvgTimeUtt = mean(AvgTimeUtt,'omitnan');
